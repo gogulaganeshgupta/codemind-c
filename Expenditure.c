e58076165a4eb4201b83a2fa71188cb17cf67f94
gogulaganeshgupta <120078187+gogulaganeshgupta@users.noreply.github.com>
2022-12-20 20:34:50 +0530

    #include<stdio.h> int main() {     int x,y,c;     scanf("%d%d",&x,&y);     c=x/y;     if(c>=30)     {         printf("YES");     }     else     {         printf("NO");     }
    }

---
+++ Expenditure.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,c;
+    scanf("%d%d",&x,&y);
+    c=x/y;
+    if(c>=30)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+}
