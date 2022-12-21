0a18cb535e37b76f279b150f444b73e24c6d075f
gogulaganeshgupta <120078187+gogulaganeshgupta@users.noreply.github.com>
2022-12-21 20:16:02 +0530

    #include<stdio.h> int main() {     int u,sr,tc;     float k;     scanf("%d",&u);     if(u<=199)     {         tc=u*1.20;         k=tc+100;     }     else if(u>=200&&u<400)     {         tc=u*1.50;         k=tc+100;     }     else if(u>=400&&u<600)     {         tc=u*1.80;         k=tc+tc*15/100;     }     else if(u>=600)     {         tc=u*2;         k=tc+tc*15/100;     }     printf("%.2f",k); }

---
+++ Current_bill.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int u,sr,tc;
+    float k;
+    scanf("%d",&u);
+    if(u<=199)
+    {
+        tc=u*1.20;
+        k=tc+100;
+    }
+    else if(u>=200&&u<400)
+    {
+        tc=u*1.50;
+        k=tc+100;
+    }
+    else if(u>=400&&u<600)
+    {
+        tc=u*1.80;
+        k=tc+tc*15/100;
+    }
+    else if(u>=600)
+    {
+        tc=u*2;
+        k=tc+tc*15/100;
+    }
+    printf("%.2f",k);
+}
